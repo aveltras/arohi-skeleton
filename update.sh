da36ba8a25b024a33671d672429ba1a833cdba37
aveltras <romain.viallard@outlook.fr>
2019-11-24 12:26:19 +0100

    Add automatic commit message with update script

--- update.sh
+++ update.sh
@@ -8,3 +8,5 @@ cp -r $TMP_DIR/skeleton $DIR/app
 mv $DIR/app/skeleton.cabal $DIR/app/app.cabal
 sed -i 's/skeleton/app/g' $DIR/app/app.cabal
 rm -rf $TMP_DIR
+git add -u app/
+git commit -m 'Bring up to date with reflex-toolkit repo'
